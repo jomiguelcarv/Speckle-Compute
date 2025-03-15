43b7a93971c328413df0e8bb4c8ad5959a962396
Rodrigo Aguirre Pereira <rodrigo.aguirre@iaac.net>
2025-03-15 01:33:47 +0100

    initial set up
    changed addresses, has one extra input, changed ghx file, fly not working

--- fetch.py
+++ fetch.py
@@ -7,9 +7,10 @@ def send_to_compute():
     #Credentials
     compute_rhino3d.Util.url = "http://3.78.144.62/"
     compute_rhino3d.Util.apiKey = "SpeckleCompute02"
-    send_to_compute_stream = "https://macad.speckle.xyz/streams/26d9a49958"
-    receive_from_compute_stream = "https://macad.speckle.xyz/streams/a63c4e60ed"
-    gh_definition = "example.ghx"
+    send_to_compute_stream = "https://macad.speckle.xyz/streams/28a211b286/models/c6a5549e3b" #check for stream or projects??
+    sendModel_to_compute_stream = "https://macad.speckle.xyz/streams/28a211b286/models/4e98b63e56" 
+    receive_from_compute_stream = "https://macad.speckle.xyz/streams/28a211b286/models/f2dd4b4cca" #check for stream or projects??
+    gh_definition = "HyperB_Data_GH_Residential-metrics_RA.ghx"
 
     gh_data = open(gh_definition, mode="r", encoding="utf-8-sig").read()
     data_bytes = gh_data.encode("utf-8")
@@ -21,12 +22,23 @@ def send_to_compute():
         "pointer": None,
         "values": [
             {
-                "ParamName": "RH_IN:Send",
+                "ParamName": "RH_IN:SendData",
                 "InnerTree": {
                     "{ 0; }": [
                         {
                             "type": "String",
                             "data": send_to_compute_stream
+                        }
+                    ]
+                }
+            }, 
+                        {
+                "ParamName": "RH_IN:SendGeo",
+                "InnerTree": {
+                    "{ 0; }": [
+                        {
+                            "type": "String",
+                            "data": sendModel_to_compute_stream
                         }
                     ]
                 }
